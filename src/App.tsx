adbf8d4f6f147998dc319717b415c00224b3f097
Julio Velezmoro <julio28ve@gmail.com>
2022-12-15 22:41:15 +0000

    Body Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
 import './App.css';
+import Body from './components/Body/Body';
 
 function App() {
   return (
     <div className="App">
-   
+      <Body>
+        
+      </Body>
     </div>
   );
 }
---
+++ src/components/Body/Body.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Body = (props:{children:React.ReactNode}) => {
+  return (
+    <div>{props.children}</div>
+  )
+}
+
+export default Body
